3d2db37fc9e4c94eeea7f90bf6ae96d386a71318
markobezic <marko.bezic@gmail.com>
2021-05-01 13:57:43 -0400

    set up routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,31 @@ import "./App.css";
 import React from "react";
 import Header from "./components/Header";
 import Courses from "./components/Courses";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import CreateCourse from "./components/CreateCourse";
+import UpdateCourse from "./components/UpdateCourse";
+import CourseDetail from "./components/CourseDetail";
+import UserSignIn from "./components/UserSignIn";
+import UserSignOut from "./components/UserSignOut";
+import UserSignUp from "./components/UserSignUp";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <Courses />
-    </>
+
+      <Switch>
+        <Route exact path="/" component={Courses} />
+        <Route path="/courses/create" component={CreateCourse} />
+        <Route path="/courses/:id/update" component={UpdateCourse} />
+        <Route path="/courses/:id" component={CourseDetail} />
+        <Route path="/signin" component={UserSignIn} />
+        <Route path="/signup" component={UserSignUp} />
+        <Route path="/signout" component={UserSignOut} />
+
+        <Route />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ client/src/components/UserSignIn.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserSignIn = () => {
+  return <div>Hello User Sign In</div>;
+};
+
+export default UserSignIn;
