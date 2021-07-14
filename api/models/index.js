d27ec90bb53cad54df12a60ecc035d54a8c2f2b3
markobezic <marko.bezic@gmail.com>
2021-07-14 12:26:36 -0400

    remove sql and connect to postgres db

--- api/models/index.js
+++ api/models/index.js
@@ -1,5 +1,6 @@
 "use strict";
 
+require("dotenv").config();
 const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
@@ -8,15 +9,20 @@ const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
+console.log("Console:", process.env);
+
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
+    process.env.DATABSE,
+    process.env.USERNAME,
+    process.env.PASSWORD,
+    {
+      host: process.env.HOST,
+      dialect: process.env.DIALECT,
+    }
   );
 }
 
